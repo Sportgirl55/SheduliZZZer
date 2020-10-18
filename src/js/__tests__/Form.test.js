0c8dc44fb8af197ad4dd6b32cad723a43104c7b3
Andrey Khomutov <homutovan@live.com>
2020-10-18 18:35:50 +0700

    initial commit

--- src/js/__tests__/Form.test.js
+++
@@ -1,8 +0,0 @@
-import Form from '../Form';
-
-const element = 'element';
-const form = new Form(element);
-
-test('create form', () => {
-  expect(form.element).toBe(element);
-});
--- src/js/app.js
+++
@@ -1,4 +0,0 @@
-import Form from './Form';
-
-/* eslint no-unused-vars: 0 */
-const form = new Form();
